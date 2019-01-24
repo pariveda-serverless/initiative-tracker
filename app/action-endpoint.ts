10361013c6c1df02edeae07113a916fd5abae3ce
Phillip <phillip.manwaring@gmail.com>
2019-01-23 22:56:42 -0500

    update handle actions method

--- app/action-endpoint.ts
+++ app/action-endpoint.ts
@@ -1,6 +1,6 @@
-import { apiWrapper } from '@manwaring/lambda-wrapper';
+import { apiWrapper, ApiSignature } from '@manwaring/lambda-wrapper';
 
-export const handler = apiWrapper(async (body, success, error) => {
+export const handler = apiWrapper(async ({ body, success, error }: ApiSignature) => {
   try {
     console.log(body);
     success(body);
