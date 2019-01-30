ccaf96bc68d0db794c38e9a9fefd2add62b10ece
Jorge Diaz-Ortiz <diazjorge1995@gmial.com>
2019-01-30 11:16:20 -0500

    Added promise tothe join initiative handler.

--- app/action-endpoint.ts
+++ app/action-endpoint.ts
@@ -28,30 +28,30 @@ export const handler = apiWrapper(async ({body, success, error} : ApiSignature) 
   }
 });
 
-const joinInitiativeHalder = apiWrapper(async ({ body, success, error }: ApiSignature) => {
-  try {
-
-    const payload = JSON.parse(body.payload);
-    const selection = payload.actions[0].value;
-    const role = selection.split(':')[0];
-    const initiativeId = selection.split(':')[1];
-    console.log('Payload', payload);
-
-    const user = new User ({
-      slackId: payload.user.id,
-      role: role,
-    })
-
-    await joinInitiative(initiativeId, user);
-
-    const message = {
-      text: `User ${user.slackId}  joined initiative ${initiativeId} as a ${user.role}!`,
-      response_type: 'in_channel'
-    };
-
-    console.log('Message is ', JSON.stringify(message));
-    success(message);
-  } catch (err) {
-    error(err);
-  }
-});
+const joinInitiativeHalder = async (body: any): Promise<any> => {
+  return new Promise(async (resolve, reject) =>{
+    try {
+      const payload = JSON.parse(body.payload);
+      const selection = payload.actions[0].value;
+      const role = selection.split(':')[0];
+      const initiativeId = selection.split(':')[1];
+      console.log('Payload', payload);
+  
+      const user = new User ({
+        slackId: payload.user.id,
+        role: role,
+      })
+  
+      await joinInitiative(initiativeId, user);
+  
+      const message = {
+        text: `User ${user.slackId}  joined initiative ${initiativeId} as a ${user.role}!`,
+        response_type: 'in_channel'
+      };
+      console.log('Message is ', JSON.stringify(message));
+      resolve(message);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
