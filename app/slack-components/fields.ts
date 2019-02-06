e661c2b885d61da2ab197dd86b2bf1e980e6d77b
Phillip <phillip.manwaring@gmail.com>
2019-02-05 23:28:45 -0500

    remove unused files

--- app/slack-components/fields.ts
+++
@@ -1,6 +0,0 @@
-// import { Types } from './types'
-// import { Colors } from './display'
-
-// export function generateField(title: string, value: string, short?: boolean): any {
-//   return { title, value, short}
-// }
