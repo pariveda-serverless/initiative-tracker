fedf56f15d56837b010814fccbc20a795cad612f
Phillip <phillip.manwaring@gmail.com>
2019-02-09 01:05:58 -0500

    Update create-initiative-p.ts

--- app/create-initiative-p.ts
+++ app/create-initiative-p.ts
@@ -6,19 +6,7 @@ const initiatives = new DynamoDB.DocumentClient({ region: process.env.REGION });
 
 export const handler = apiWrapper(async ({ body, success, error }: ApiSignature) => {
   try {
-    console.log(typeof body);
-    console.log(body.text);
-    console.log(typeof body.text);
-    console.log('body');
-    console.log(body);
-    const testString =
-      'serverless workshops, making content that can be shared with clients and will help us learn ourselves';
-    console.log(typeof testString);
-    const splitTestString = testString.split(',');
-    console.log(splitTestString);
-    const [first, second] = testString.split(',');
-    console.log(first);
-    const [name, description] = body.text.split[','];
+    const [name, description] = body.text.split(',');
     const initiative = new CreateInitiativeRequest({ name, description });
     await saveInitiative(initiative);
     const message = {
