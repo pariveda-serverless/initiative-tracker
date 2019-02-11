22e4d8f4f04ad016fb1b01df68dd7ec792bb1120
Phillip <phillip.manwaring@gmail.com>
2019-02-10 23:54:57 -0500

    update chron job to run every 5 minutes for testing

--- app/request-feedback.ts
+++ app/request-feedback.ts
@@ -2,6 +2,8 @@ import { snsWrapper, SnsSignature } from '@manwaring/lambda-wrapper';
 
 export const handler = snsWrapper(async ({ message, success, error }: SnsSignature) => {
   try {
+    // TODO get initiative information (including champions and members)
+    // Send status update requests (to champions only?)
     success(message);
   } catch (err) {
     error(err);
