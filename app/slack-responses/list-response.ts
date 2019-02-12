4b819ccbe9758f24fd6af908758d7eadb1d74662
Phillip <phillip.manwaring@gmail.com>
2019-02-12 17:09:57 -0500

    update list response when no initiatives found
    fixes #19

--- app/slack-responses/list-response.ts
+++ app/slack-responses/list-response.ts
@@ -3,10 +3,12 @@ import { InitiativeResponse } from '../initiative';
 import { BasicInitiativeCard } from './initiative-card';
 
 export class ListResponse implements Message {
+  text: string;
   response_type: 'in_channel' | 'ephemeral';
   attachments: Attachment[];
   constructor(initiatives: InitiativeResponse[]) {
     this.response_type = 'ephemeral';
+    this.text = initiatives && initiatives.length ? '' : 'No initiatives found matching the search criteria';
     this.attachments = initiatives.map(initiative => new BasicInitiativeCard(initiative, false));
   }
 }
