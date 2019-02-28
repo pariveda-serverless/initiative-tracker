752800602c7d34c7f36dddc2cad0006bd7aa2b96
Joseph Monroe <joseph.monroe@parivedasolutions.com>
2019-02-28 15:14:01 -0500

    updated action check

--- app/action-endpoint.ts
+++ app/action-endpoint.ts
@@ -26,6 +26,12 @@ const initiatives = new DynamoDB.DocumentClient({ region: process.env.REGION });
 
 export const handler = apiWrapper(async ({ body, success, error }: ApiSignature) => {
   try {
+    // const payload: Payload = JSON.parse(body.payload);
+    // const teamId = payload.team.id;
+    // const responseUrl = payload.response_url;
+    // const channel = payload.channel.id;
+    // const action = payload.actions ? payload.actions[0].action_id : payload.callback_id;
+    // const triggerId = payload.trigger_id;
     let dialogResponse = false;
     let dialogError = false;
     let response: Message | EditInitiativeDialogResponse | EditInitiativeFieldValidator;
@@ -127,6 +133,16 @@ export const handler = apiWrapper(async ({ body, success, error }: ApiSignature)
   }
 });
 
+function getFieldsFromBody(body: any) {
+  const payload: Payload = JSON.parse(body.payload);
+  const teamId = payload.team.id;
+  const responseUrl = payload.response_url;
+  const channel = payload.channel.id;
+  const action =  payload.actions ? payload.actions[0].action_id : payload.callback_id;
+  const triggerId = payload.trigger_id;
+  return { payload, teamId, responseUrl, channel, action, triggerId };
+}
+
 function updateInitiativeNameAndDescription(
   teamId: string,
   initiativeId: string,
@@ -145,16 +161,6 @@ function updateInitiativeNameAndDescription(
   };
   console.log('Updating Initiative Name and/or Description', params);
   return initiatives.update(params).promise();
-}
-
-function getFieldsFromBody(body: any) {
-  const payload: Payload = JSON.parse(body.payload);
-  const teamId = payload.team.id;
-  const responseUrl = payload.response_url;
-  const channel = payload.channel.id;
-  const action = payload.actions[0].action_id;
-  const triggerId = payload.trigger_id;
-  return { payload, teamId, responseUrl, channel, action, triggerId };
 }
 
 async function updateInitiativeStatus(initiativeId: string, teamId: string, status: Status): Promise<any> {
