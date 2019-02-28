35997811b5160208c1a4cd8a112ec1c996e6bc56
Phillip <phillip.manwaring@gmail.com>
2019-02-27 21:54:23 -0500

    update detection of member records

--- app/stream-updates.ts
+++ app/stream-updates.ts
@@ -1,13 +1,17 @@
 import { SNS } from 'aws-sdk';
 import { StreamSignature, streamWrapper } from '@manwaring/lambda-wrapper';
 import { MEMBER_TYPE, MemberResponse } from './member';
+import { InitiativeRecord } from './initiative';
 
 const sns = new SNS({ apiVersion: '2010-03-31' });
 
 export const handler = streamWrapper(async ({ versions, success, error }: StreamSignature) => {
   try {
     const newMembers = versions
-      .filter(version => version.newVersion.indexOf(MEMBER_TYPE) > -1 && !version.oldVersion)
+      .filter(version => {
+        const record = <InitiativeRecord>version.newVersion;
+        return record.identifiers.indexOf(MEMBER_TYPE) > -1 && !version.oldVersion;
+      })
       .map(record => new MemberResponse(record));
     await Promise.all(newMembers.map(member => publishNewMembersForNotifications(member)));
     success();
