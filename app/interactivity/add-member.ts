863e95936abbed752a2f1b0f4387cc425e982446
Phillip Manwaring <phillip.manwaring@gmail.com>
2019-03-20 13:37:52 -0400

    implement member joining

--- app/interactivity/add-member.ts
+++ app/interactivity/add-member.ts
@@ -5,13 +5,12 @@ import { DetailResponse } from '../slack-messages';
 import { getChannelInfo } from '../slack-api';
 import { getInitiativeIdentifiers } from '../initiatives';
 import { initiativesTable } from '../shared';
+import { joinInitiative } from './join-initiative';
 
 export async function addMemberAction(teamId: string, channel: string, payload: ActionPayload): Promise<Message> {
-  const slackUserId = payload.user.id;
-  const { member } = payload.submission;
+  const { member: slackUserId } = payload.submission;
   const { initiativeId } = parseValue(payload.state);
-  // TODO add member to initiative
-  // await updateInitiative(teamId, initiativeId, name, description, status, channelId);
+  await joinInitiative(teamId, initiativeId, slackUserId, false);
   const initiative = await getInitiativeDetails(teamId, initiativeId);
   return new DetailResponse(initiative, slackUserId, channel);
 }
