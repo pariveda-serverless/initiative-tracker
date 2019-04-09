8b2d5d4fef8a1639a97d796dd4803cba3544fde7
Phillip <phillip.manwaring@gmail.com>
2019-04-08 23:18:54 -0400

    remove debugs

--- app/interactivity/get-initiative-list.ts
+++ app/interactivity/get-initiative-list.ts
@@ -11,15 +11,10 @@ export async function getInitiativeListAction(
 ): Promise<Message> {
   const slackUserId = payload.user.id;
   let status, office;
-  console.log(payload.actions && payload.actions.length > 0);
   if (payload.actions && payload.actions.length > 0) {
-    console.log(payload.actions[0].selected_option.value);
     ({ status, office } = parseValue(payload.actions[0].selected_option.value));
-    console.log(status, office);
   }
-  console.log(status, office);
   const query = new Query({ status, office });
-  console.log(query);
   const initiatives = await getInitiatives(teamId, query);
   return new ListResponse({ initiatives, channelId, slackUserId, query });
 }
