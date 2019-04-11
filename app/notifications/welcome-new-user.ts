2778d66ae2b03a2f61d8c547a558147f51dc7dd1
Phillip <phillip.manwaring@gmail.com>
2019-04-10 22:00:30 -0400

    Update welcome-new-user.ts

--- app/notifications/welcome-new-user.ts
+++ app/notifications/welcome-new-user.ts
@@ -1,11 +1,13 @@
 import { snsWrapper, SnsSignature } from '@manwaring/lambda-wrapper';
 import { WelcomeMessage } from '../slack-messages/welcome';
 import { sendMessage } from '../slack-api';
+import { User } from '../users';
 
-export const handler = snsWrapper(async ({ message: user, success, error }: SnsSignature) => {
+export const handler = snsWrapper(async ({ message, success, error }: SnsSignature) => {
   try {
+    const user = <User>message;
     const welcome = new WelcomeMessage(user);
-    await sendMessage(welcome, user.teamdId);
+    await sendMessage(welcome, user.teamId);
     success();
   } catch (err) {
     error(err);
