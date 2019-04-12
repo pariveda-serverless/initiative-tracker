6cf42ee0fa891bd780f5f914d4feeae15fd01164
Phillip <phillip.manwaring@gmail.com>
2019-04-11 23:07:08 -0400

    update welcome message

--- app/slack-messages/welcome.ts
+++ app/slack-messages/welcome.ts
@@ -15,23 +15,35 @@ class Welcome implements Section {
   type: 'section' = 'section';
   text: MarkdownText;
   constructor(user: User) {
-    const text = `Welcome to Initiative Tracker, ${user.name}!
+    const text = `Welcome to Initiative Tracker, ${getFirstName(user)}!
     Here's some tips and tricks to help you get started :female-teacher:
+
 
     To add a new initiative you can use the */add-initiative* command like this:
     */add-initiative [name], [optional description], [optional #channel]* :ok_hand:
 
+
     Want to see all of the initiatives?  Use the */show-initiatives* command
     */show-initiatives [optional filter: 'public'], [optional filter: 'active' | 'abandoned' | 'on hold' | 'complete']*
-    From the list view you'll be able to filter further by status and office
+    
+    
+    From the list view you'll be able to filter further by status and office, and also view an initiative's details, including who the champions and members are and what Slack channel the group uses for communicating.  You'll also be able to edit initiative information in case you made a mistake or something changes.
 
-    From the list you can view the details of an initiative including who the champions and members are and what Slack channel the group uses for communicating.  You'll also be able to edit initiative information in case you made a mistake or something changes.
 
     That's all for now - best of luck and thanks for getting involved with internal initiatives!
     
+
     
     Made with :black_heart: by Pariveda New York :city_sunset:
     `.replace(/  +/g, '');
     this.text = { type: 'mrkdwn', text };
+  }
+}
+
+function getFirstName(user: User): string {
+  if (user.name) {
+    return user.name.indexOf(' ') >= 0 ? user.name.split(' ')[0] : user.name;
+  } else {
+    return '';
   }
 }
