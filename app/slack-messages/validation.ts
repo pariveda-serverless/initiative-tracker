da128b1de79788cb0488422ff39ffaa5dd19d0b9
Phillip <phillip.manwaring@gmail.com>
2019-04-13 00:07:57 -0400

    update message when no name given

--- app/slack-messages/validation.ts
+++ app/slack-messages/validation.ts
@@ -4,7 +4,7 @@ import { Profile } from '../slack-api';
 export class InvalidAddResponse implements Message {
   blocks: (Section | DividerBlock | Action | ContextBlock)[];
   constructor(text: string, profile: Profile) {
-    const nameMissing = new NameMissing(text);
+    const nameMissing = new NameMissing();
     this.blocks = [nameMissing];
   }
 }
@@ -12,10 +12,10 @@ export class InvalidAddResponse implements Message {
 class NameMissing implements Section {
   type: 'section' = 'section';
   text: MarkdownText;
-  constructor(command: string) {
-    const text = `Oh no, it looks like you forgot to name your initiative!
-    You used the slash command */add-initiative ${command}*
-    The correct command is */add-initiative [name], [optional description], [optional #channel]*`.replace(/  +/g, '');
+  constructor() {
+    const text = `Oh no, it looks like you forgot to name your initiative! :crying_cat_face:
+    In addition to the name you can also add a description and channel to make it easier for people to learn more about the initiative and see if it's something they want to join
+    */add-initiative [name], [optional description], [optional #channel]*`.replace(/  +/g, '');
     this.text = { type: 'mrkdwn', text };
   }
 }
