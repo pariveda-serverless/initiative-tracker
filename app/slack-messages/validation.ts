951e9b2dd9ad7e90b49cdf1ec5d7e707560b03b4
Phillip <phillip.manwaring@gmail.com>
2019-04-13 00:31:30 -0400

    Update validation.ts
    fix #36

--- app/slack-messages/validation.ts
+++ app/slack-messages/validation.ts
@@ -14,6 +14,7 @@ class NameMissing implements Section {
   text: MarkdownText;
   constructor() {
     const text = `Oh no, it looks like you forgot to name your initiative! :crying_cat_face:
+    
     In addition to the name you can also add a description and channel to make it easier for people to learn more about the initiative and see if it's something they want to join
     */add-initiative [name], [optional description], [optional #channel]*`.replace(/  +/g, '');
     this.text = { type: 'mrkdwn', text };
