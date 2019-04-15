8b201771dc2c3a901cb3609ed663e4d4b1904a7e
Phillip Manwaring <phillip.manwaring@gmail.com>
2019-04-15 15:27:41 -0400

    use new view for list as well as details

--- app/slack-messages/shared-messages/initiatives.ts
+++ app/slack-messages/shared-messages/initiatives.ts
@@ -165,7 +165,7 @@ class BasicInitiativeOverview implements MarkdownText {
     const channel = initiative.channel && initiative.channel.parsed ? ` @ ${initiative.channel.parsed}` : '';
     const name = initiative.name ? `*${initiative.name}${channel}*` : '';
     const description = initiative.shortDescription ? `\n>_${initiative.shortDescription}_` : '';
-    this.text = name + channel + description;
+    this.text = name + description;
   }
 }
 
@@ -173,9 +173,9 @@ class DetailedInitiativeOverview implements MarkdownText {
   type: 'mrkdwn' = 'mrkdwn';
   text: string;
   constructor(initiative: Initiative) {
-    const name = initiative.name ? `*${initiative.name}*` : '';
-    const channel = initiative.channel && initiative.channel.parsed ? `* @ ${initiative.channel.parsed}*` : '';
+    const channel = initiative.channel && initiative.channel.parsed ? ` @ ${initiative.channel.parsed}` : '';
+    const name = initiative.name ? `*${initiative.name}${channel}*` : '';
     const description = initiative.description ? `\n>_${initiative.description}_` : '';
-    this.text = name + channel + description;
+    this.text = name + description;
   }
 }
